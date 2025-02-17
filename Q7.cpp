c4267c847f5cfb6efad5322c536112fe8cadc2b6
Vikas Kumar Jha <vikaskumarjha763@gmail.com>
2025-02-17 09:52:07 +0530

    Initial Commit

---
+++ Q7.cpp
@@ -0,0 +1,42 @@
+//Date: 17/2/25
+#include <iostream>
+using namespace std;
+void insertion(int arr[],int n)
+{
+    int c=0,shift=0;
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>key)
+        {
+            c++;
+            arr[j+1]=arr[j];
+            shift++;
+            j=j-1;
+            
+        }
+        arr[j+1]=key;
+    }
+    cout<<"Comparsion"<<c<<endl;
+    cout<<"Shift"<<shift<<endl;
+    cout<<"Array";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cout <<"Enter the size:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    insertion(arr,n);
+    
+}
